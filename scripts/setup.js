5f436be0e5ee1115beed348eb2f60d6cadf38521
Antonio <bucchiarone@fbk.eu>
2022-12-01 07:21:48 +0100

    changes in the CI

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,29 +1,29 @@
 require('dotenv').config()
-var Student   = require('../app/models/student'); // get our mongoose model
+var Student = require('../app/models/student'); // get our mongoose model
 
-var mongoose    = require('mongoose');
+var mongoose = require('mongoose');
 // connect to database
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then ( () => {
-	console.log("Connected to Database")
-});
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Connected to Database")
+	});
 
 // Clear users
-Student.remove().then( () => {
-	var marco = new Student({ 
-		email: 'marco@unitn.com',
+Student.remove().then(() => {
+	var antonio = new Student({
+		email: 'antbucc@gmail.com',
 		password: '123'
 	});
-	return marco.save();
-}).then( () => {
-	console.log('User marco@unitn.com saved successfully');
-}).then( () => {
-	var mario = new Student({ 
+	return antonio.save();
+}).then(() => {
+	console.log('User antbucc@gmail.com saved successfully');
+}).then(() => {
+	var mario = new Student({
 		email: 'mario.rossi@unitn.com',
 		password: '123'
 	});
 	return mario.save();
-}).then( () => {
+}).then(() => {
 	console.log('User mario.rossi@unitn.com saved successfully');
 	process.exit();
 });
