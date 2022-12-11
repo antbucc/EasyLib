0a366cd7524026ccf78818c2d0a665b649e39e50
Antonio <bucchiarone@fbk.eu>
2022-12-11 19:39:24 +0100

    changes

--- app/booklendings.test.js
+++ app/booklendings.test.js
@@ -1,33 +1,34 @@
 /**
  * https://www.npmjs.com/package/supertest
  */
-const request  = require('supertest');
-const app      = require('./app');
-const jwt      = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const request = require('supertest');
+const app = require('./app');
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 describe('GET /api/v1/booklendings', () => {
 
   let connection;
 
-  beforeAll( async () => {
+  beforeAll(async () => {
     jest.setTimeout(8000);
     jest.unmock('mongoose');
-    connection = await  mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+    connection = await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Database connected!');
     //return connection; // Need to return the Promise db connection?
   });
 
-  afterAll( () => {
+  afterAll(() => {
     mongoose.connection.close(true);
     console.log("Database connection closed");
   });
-  
+
   // create a valid token
   var token = jwt.sign(
-    {email: 'John@mail.com'},
+    { email: 'John@mail.com' },
     process.env.SUPER_SECRET,
-    {expiresIn: 86400}
+    { expiresIn: 86400 }
   );
 
   test('POST /api/v1/booklendings with Student not specified', () => {
@@ -37,7 +38,7 @@ describe('GET /api/v1/booklendings', () => {
       .set('Accept', 'application/json')
       .expect(400, { error: 'Student not specified' });
   });
-  
+
   test('POST /api/v1/booklendings with Book not specified', () => {
     return request(app)
       .post('/api/v1/booklendings')
@@ -46,7 +47,7 @@ describe('GET /api/v1/booklendings', () => {
       .send({ student: 'whatever' }) // sends a JSON post body
       .expect(400, { error: 'Book not specified' });
   });
-  
+
   test('POST /api/v1/booklendings Student does not exist', () => {
     return request(app)
       .post('/api/v1/booklendings')
@@ -55,13 +56,13 @@ describe('GET /api/v1/booklendings', () => {
       .send({ student: '/api/v1/students/111', book: '/api/v1/books/0' }) // sends a JSON post body
       .expect(400, { error: 'Student does not exist' });
   });
-  
+
   test('POST /api/v1/booklendings Book does not exist', () => {
     return request(app)
       .get('/api/v1/students')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then( (res) => {
+      .then((res) => {
         return request(app)
           .post('/api/v1/booklendings')
           .set('x-access-token', token)
