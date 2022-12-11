f3c2d632fcb8b8b0d259a9f458f8f0fb9767096c
Antonio <bucchiarone@fbk.eu>
2022-12-11 18:57:42 +0100

    new version

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ const port = process.env.PORT || 8080;
  * Configure mongoose
  */
 // mongoose.Promise = global.Promise;
-console.log(process.env.DB_URL);
-app.locals.db = mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-
-        console.log("Connected to Database");
-
-        app.listen(port, () => {
-            console.log(`Server listening on port ${port}`);
-        });
-
+app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then ( () => {
+    
+    console.log("Connected to Database");
+    
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
     });
+    
+});
